4633397fa34cfef7373f42cb6099cbed9d949362
MartaReb <49365894+MartaReb@users.noreply.github.com>
2025-03-25 19:41:47 +0100

    Create project_3.py

---
+++ Day 3 - The Ultimate Wacky Recipe Maker/project_3.py
@@ -0,0 +1,8 @@
+food = input("Name a food: ")
+plant = input("Name a plant: ")
+cooking = input("Name a method of cooking: ")
+burned = input("What word describes burned food? ")
+item = input("Name a household item: ")
+print()
+print("MENU")
+print(cooking, food, "with", burned, plant, "on a bed of", item)
